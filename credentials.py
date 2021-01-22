8c3bd1865d5649343750949bbe98ed6041f2db93
altayinci <incis@itu.edu.tr>
2021-01-22 13:37:55 +0300

    deneme

--- credentials.py
+++ credentials.py
@@ -1,5 +1,6 @@
 credentials = {
     'client_id': "*******",
     'client_secret': "*******",
-    'base_url': "https://api.spotify.com/v1/"
+    'base_url': "https://api.spotify.com/v1/",
+   'deneme':"deneme"
 }
